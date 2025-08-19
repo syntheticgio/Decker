54994a7b1001b9bf7a25a6e8ee4ca32d21b62be6
John Torcivia <syntheticgio@aol.com>
2025-08-19 17:53:34 -0400

    Fixed rendering location

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the 'js' directory
-app.use(express.static(path.join(__dirname, 'js')));
+// Serve static files from the 'js/build' directory
+app.use(express.static(path.join(__dirname, 'js/build')));
 
 app.listen(port, '127.0.0.1', () => {
-  console.log(`Decker is live at http://127.0.0.1:${port}/build/decker.html`);
+  console.log(`Decker is live at http://127.0.0.1:${port}/decker.html`);
 });
